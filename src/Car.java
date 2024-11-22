a8984b6b38c0e05c9d2dec0ddf7fa546012ba7e3
Salma884 <147331892+Salma884@users.noreply.github.com>
2024-11-22 12:39:48 +0200

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,34 @@
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Car implements Runnable{
+class Car implements Runnable {
+    private int id;
+    private int enterTime;
+    private int exitTime;
+    private Gate gate;
+
+    private ParkingLot parkLot;
+
+    public Car(int id, int enterTime, int exitTime, Gate gate) {
+        this.id = id;
+        this.exitTime = exitTime;
+        this.gate = gate;
+        this.enterTime = enterTime;
+    }
+
+    @Override
+    public void run() {
+            gate.processCar(parkLot);
+    }
+    public int getArrivalTime(){
+        return enterTime;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getParkingDuration(){
+        return exitTime;
+    }
 }
