3c6b07eeb4e634cce68e0bd85c27ae61b1df8d09
blackZ0 <150898184+blackZ0@users.noreply.github.com>
2024-11-22 14:01:45 +0200

    Update parkLot.java

--- src/parkLot.java
+++ src/parkLot.java
@@ -1,5 +1,30 @@
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.ThreadLocalRandom;
 
-public class parkLot {
+public class ParkingLot {
+    private final Semaphore parkingSpots;
+    private int totalCarsServed = 0;
+
+    public ParkingLot(int spots) {
+        parkingSpots = new Semaphore(spots);
+    }
+
+    public boolean tryToPark() throws InterruptedException {
+        return parkingSpots.tryAcquire();
+    }
+
+    public void leaveParking() {
+        parkingSpots.release();
+    }
+
+    public synchronized void incrementCarsServed() {
+        totalCarsServed++;
+    }
+
+    public int getTotalCarsServed() {
+        return totalCarsServed;
+    }
+
+    public int getAvailableSpots() {
+        return parkingSpots.availablePermits();
+    }
 }
